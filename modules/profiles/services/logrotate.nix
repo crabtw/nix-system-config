ca9cf914c9376e0895dd7cbc8ff137a512cdc252
Jyun-Yan You <jyyou.tw@gmail.com>
2022-01-06 08:54:15 +0800

    Remove duplicated logrotate entries

--- modules/profiles/services/logrotate.nix
+++ modules/profiles/services/logrotate.nix
@@ -1,24 +1,5 @@
 {
   services.logrotate = {
     enable = true;
-    extraConfig = ''
-      weekly
-      rotate 4
-      create
-
-      /var/log/wtmp {
-          monthly
-          create 0664 root utmp
-          minsize 1M
-          rotate 1
-      }
-
-      /var/log/btmp {
-          missingok
-          monthly
-          create 0660 root utmp
-          rotate 1
-      }
-    '';
   };
 }
