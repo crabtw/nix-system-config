3a39953df90870561ed59f713645822b0334ad34
Jyun-Yan You <jyyou.tw@gmail.com>
2022-03-04 15:17:37 +0800

    Bump my-vim-plugin

--- pkgs/my-vim-plugin.nix
+++ pkgs/my-vim-plugin.nix
@@ -2,11 +2,11 @@
 
 buildVimPluginFrom2Nix {
   pname = "my-vim";
-  version = "2022-02-25";
+  version = "2022-03-04";
   src = fetchFromGitHub {
     owner = "crabtw";
     repo = "my.vim";
-    rev = "9b7817739ccc34d04a79b43d5be9aa182e1c49db";
-    sha256 = "1z2mgabnazv1v69kry68qf7hd49wcgs24biraf3j9v77zbhah7y8";
+    rev = "7e66073aa8326f742bca01f381d9cce6018bc856";
+    sha256 = "03ifvqanysdaf6igzns8hcy1c3yzs97wxgpybsm44n2whm8prbvr";
   };
 }
