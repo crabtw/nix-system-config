4c00b9984c5568f5a7450988de2e5a6b7f6ebbc0
Jyun-Yan You <jyyou.tw@gmail.com>
2024-02-03 10:58:18 +0800

    Revert "Update wezterm config"
    This reverts commit fd6e92df5987750cebdd66cc0c60ff39ae9d9bd3.

--- home/modules/profiles/desktop.nix
+++ home/modules/profiles/desktop.nix
@@ -54,6 +54,7 @@
     enableBashIntegration = true;
     extraConfig = ''
       local my_default = wezterm.color.get_default_colors()
+      my_default.foreground = "light grey"
       my_default.brights[5] = "#6464ff"
 
       return {
