9f3f314d314d5ec4a22b63d4632e919c441b764a
Jyun-Yan You <jyyou.tw@gmail.com>
2025-10-24 11:18:24 +0800

    Fix fcitx5-qt6

--- overlays/local.nix
+++ overlays/local.nix
@@ -42,4 +42,27 @@ in
     );
 
   wstunnel-bin = prev.callPackage ../pkgs/wstunnel-bin.nix { };
+
+  qt6Packages = prev.qt6Packages // {
+    fcitx5-qt = prev.qt6Packages.fcitx5-qt.overrideAttrs (
+      finalAttrs: prevAttrs:
+      assert !(prevAttrs ? patches);
+      {
+        patches = [
+          (prev.fetchpatch2 {
+            url = "https://github.com/fcitx/fcitx5-qt/commit/46a07a85d191fd77a1efc39c8ed43d0cd87788d2.patch?full_index=1";
+            hash = "sha256-qv8Rj6YoFdMQLOB2R9LGgwCHKdhEji0Sg67W37jSIac=";
+          })
+          (prev.fetchpatch2 {
+            url = "https://github.com/fcitx/fcitx5-qt/commit/6ac4fdd8e90ff9c25a5219e15e83740fa38c9c71.patch?full_index=1";
+            hash = "sha256-x0OdlIVmwVuq2TfBlgmfwaQszXLxwRFVf+gEU224uVA=";
+          })
+          (prev.fetchpatch2 {
+            url = "https://github.com/fcitx/fcitx5-qt/commit/1d07f7e8d6a7ae8651eda658f87ab0c9df08bef4.patch?full_index=1";
+            hash = "sha256-22tKD7sbsTJcNqur9/Uf+XAvMvA7tzNQ9hUCMm+E+E0=";
+          })
+        ];
+      }
+    );
+  };
 }
