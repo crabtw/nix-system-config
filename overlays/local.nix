55d497ded6a7e01c2395db40cf973050772bcc1c
Jyun-Yan You <jyyou.tw@gmail.com>
2026-01-02 11:09:50 +0800

    Fix build

--- overlays/local.nix
+++ overlays/local.nix
@@ -42,4 +42,15 @@ in
     );
 
   wstunnel-bin = prev.callPackage ../pkgs/wstunnel-bin.nix { };
+
+  trayer = prev.trayer.overrideAttrs (
+    finalAttrs: prevAttrs: {
+      patches = [
+        (prev.fetchpatch {
+          url = "https://aur.archlinux.org/cgit/aur.git/plain/trayer-srg-trayer-1.1.8-bg_init_prototype.patch?h=trayer";
+          hash = "sha256-wLrWA/vs9A+gpoRQ/tvOv+0ip/6aS2HlL9EPx6Xnf+c=";
+        })
+      ];
+    }
+  );
 }
