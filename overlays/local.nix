e2a2366561ee16fe28176892fd76bd26b4724eb6
Jyun-Yan You <jyyou.tw@gmail.com>
2026-03-12 16:47:17 +0800

    Fix rtorrent segfault

--- overlays/local.nix
+++ overlays/local.nix
@@ -42,4 +42,34 @@ in
     );
 
   wstunnel-bin = prev.callPackage ../pkgs/wstunnel-bin.nix { };
+
+  libtorrent-rakshasa =
+    assert prev.libtorrent-rakshasa.version == "0.16.6";
+    prev.libtorrent-rakshasa.overrideAttrs (
+      finalAttrs: prevAttrs: {
+        version = "0.16.7";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "rakshasa";
+          repo = "libtorrent";
+          tag = "v${finalAttrs.version}";
+          hash = "sha256-cCUgmDiXapJw00rQlU3fcnEEIzWXy9d9UieKuxdhxBE=";
+        };
+      }
+    );
+
+  rtorrent =
+    assert prev.rtorrent.version == "0.16.6";
+    prev.rtorrent.overrideAttrs (
+      finalAttrs: prevAttrs: {
+        version = "0.16.7";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "rakshasa";
+          repo = "rtorrent";
+          tag = "v${finalAttrs.version}";
+          hash = "sha256-bgCVzsyTm3i3dgLZq02OyaOM0cJ0GjLDNp6dBES0RJI=";
+        };
+      }
+    );
 }
